0df7d219aec006c496d3797beff0f1b90fcbf37b
Timothy Liu <tlkh@users.noreply.github.com>
2018-02-02 01:05:03 +0700

    added health myths

--- js/app.js
+++ js/app.js
@@ -2,7 +2,11 @@ $(document).ready(function () {
 
     // Define cards
     var cards = [
-          new Tindercardsjs.card(0, 'Red Wine Cures Heart Disease', 'I like big butts', 'img/redwine.png'),
+        new Tindercardsjs.card(0, 'Red Wine Cures Heart Disease', 'HEALTH', 'img/redwine.png'),
+        new Tindercardsjs.card(1, 'Drink 8 Glasses of Water a Day', 'HEALTH', 'img/glasseswater.jpg'),
+        new Tindercardsjs.card(2, 'Eggs Are Bad for Your Heart', 'HEALTH', 'img/eggs.jpg'),
+        new Tindercardsjs.card(3, 'Antiperspirant Causes Breast Cancer', 'HEALTH', 'img/deo.jpg'),
+        new Tindercardsjs.card(4, 'Being Cold Gives You a Cold', 'HEALTH', 'img/cold.jpg'),
         ];
 
     // Render cards
