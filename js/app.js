7f49941876e02c9c16181d80b0356880c9db1539
Timothy Liu <tlkh@users.noreply.github.com>
2018-02-02 14:03:33 +0700

    scoring

--- js/app.js
+++ js/app.js
@@ -1,17 +1,50 @@
 $(document).ready(function () {
 
+    score = 0;
+    var score_display = document.getElementById("score_display");
+    console.log(score_display);
+    score_display.innerHTML = score.toString();
+
     // Define cards
     var cards = [
-        new tinderCards.card(0, 'Red Wine Cures Heart Disease', 'HEALTH', 'img/redwine.png'),
-        new tinderCards.card(1, 'Drink 8 Glasses of Water a Day', 'HEALTH', 'img/glasseswater.jpg'),
-        new tinderCards.card(2, 'Eggs Are Bad for Your Heart', 'HEALTH', 'img/eggs.jpg'),
-        new tinderCards.card(3, 'Antiperspirant Causes Breast Cancer', 'HEALTH', 'img/deo.jpg'),
-        new tinderCards.card(4, 'Being Cold Gives You a Cold', 'HEALTH', 'img/cold.jpg'),
+        new tinderCards.card(0, 'Red Wine Cures Heart Disease', 'HEALTH', 'img/redwine.png', false),
+        new tinderCards.card(1, 'Drink 8 Glasses of Water a Day', 'HEALTH', 'img/glasseswater.jpg', false),
+        new tinderCards.card(2, 'Eggs Are Bad for Your Heart', 'HEALTH', 'img/eggs.jpg', false),
+        new tinderCards.card(3, 'Antiperspirant Causes Breast Cancer', 'HEALTH', 'img/deo.jpg', false),
+        new tinderCards.card(4, 'Being Cold Gives You a Cold', 'HEALTH', 'img/cold.jpg', false),
         ];
 
     // Render cards
     tinderCards.render(cards, $('#main'), function (event) {
-        console.log('Swiped ' + event.direction + ', card_id is ' + event.cardid + ' and target is:');
-        console.log(event.card);
+        
+        truth = event.card_truth.toString();
+        
+        if (event.direction == 'right') {
+            console.log("swiped right");
+            console.log("true: " + event.card_truth);
+            
+            if (truth == "true"){
+                console.log("correct");
+                score++;
+            } else {
+                console.log("wrong");
+                window.alert("You got it wrong!");
+            }
+        } else {
+            console.log("swiped left");
+            console.log("true: " + event.card_truth);
+            if (truth == "false") {
+                console.log("correct");
+                score++;
+            } else {
+                console.log("wrong");
+                window.alert("You got it wrong!");
+            }
+        }
+
+        console.log(score);
+        score_display.innerHTML = score.toString();
+        
     });
+     
 });
